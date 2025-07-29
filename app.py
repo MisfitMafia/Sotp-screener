25b0293e12a87d917acb7f8a34cf75b5f17d754a
MisfitMafia <103073202+MisfitMafia@users.noreply.github.com>
2025-07-29 04:00:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def get_trades():
 
 if __name__ == '__main__':
     import os
-port = int(os.environ.get("PORT", 10000))
-app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
