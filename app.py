6a3236527a7790c06650ecc7f0712c72976c9228
MisfitMafia <103073202+MisfitMafia@users.noreply.github.com>
2025-07-30 01:59:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 from flask import Flask, jsonify
-# only if you want to load a local .env for dev:
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()
 from polygon_client import get_option_trades
 
 app = Flask(__name__)
@@ -20,6 +19,3 @@ def get_trades():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     app.run(debug=True, host="0.0.0.0", port=port)
-
-
-
